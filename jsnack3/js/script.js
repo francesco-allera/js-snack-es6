c9c4b2b35f9dd2ad82c205fc8091bfad9601945a
Francesco <francescoallera@gmail.com>
2021-01-20 16:51:07 +0100

    Exercise completed in ES6

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,28 +1,40 @@
-function myFunction(array, min, max) {
+// FUNZIONE IN ES5
+/* function myFunction(array, min, max) {
   var newArray = [];
   for (var i = min - 1; i < max; i++) {
     newArray.push(array[i]);
   }
   return newArray;
-}
-
-var mioArray = ['primo', 'secondo', 'terzo', 'quarto', 'quinto', 'sesto', 'settimo', 'ottavo', 'nono', 'decimo'];
+} */
+// FUNZIONE DI ALFREDO
+/* function myFunction (array, min, max) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] >= min && array[i] <= max) {
+      newArray.push(array[i]);
+    }
+  }
+} */
+// ESERCIZIO IN ES5
+/* var mioArray = ['primo', 'secondo', 'terzo', 'quarto', 'quinto', 'sesto', 'settimo', 'ottavo', 'nono', 'decimo'];
 
 var userMinValue = parseInt(prompt('Scegli il numero minimo'));
-
+// verificare che i controlli siano corretti prima di iniziare la funzione con il for
 var userMaxValue = parseInt(prompt('Scegli il numero massimo'));
+// verificare che i controlli siano corretti prima di iniziare la funzione con il for
 
+console.log(myFunction(mioArray, userMinValue, userMaxValue)); */
 
-console.log(myFunction(mioArray, userMinValue, userMaxValue));
-
-
+// ESERCIZIO IN ES6
 const arrayFromConst = ['primo', 'secondo', 'terzo', 'quarto', 'quinto', 'sesto', 'settimo', 'ottavo', 'nono', 'decimo'];
+const userMinValue = parseInt(prompt('Scegli il numero minimo'));
+const userMaxValue = parseInt(prompt('Scegli il numero massimo'));
 
-let newArrayFromConst = [];
+function myFunction(array, min, max) {
+  const newArray = array.filter((element, index) => {
+    return index >= (min - 1) && index <= (max - 1);
+  })
+  return newArray;
+}
 
-// arrayFromConst.map((element) => {
-//   for (let x = )
-//   newArrayFromConst.push(element);
-// });
-//
-// console.log(newArrayFromConst);
+console.log(myFunction(arrayFromConst, userMinValue, userMaxValue));
